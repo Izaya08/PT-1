ab0583748192d11fe699033873f6b199ae5d94cb
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-10-22 13:05:02 +0800

    1
    weqweqweqwewqe

--- Assets/ImpGUI.cs
+++ Assets/ImpGUI.cs
@@ -6,8 +6,31 @@ using UnityEngine;
 public class ImpGUI : MonoBehaviour
 {
     public string Labeltext = "Hello Fellow Gamer";
-    [SerializeField] private TextMeshProUGUI GUIText;
 
+    [SerializeField] private TextMeshProUGUI GUIText;
+    public int Player1attack = 100;
+    public int Player2attack = 75;
+    public int Player3attack = 60;
+
+    public int Player1Defense = 200;
+    public int Player2Defense = 225;
+    public int Player3Defense = 230;
+
+    public int Player1Speed = 50;
+    public int Player2Speed = 70;
+    public int Player3Speed = 60;
+
+    public int Player1Luck = 30;
+    public int Player2Luck = 40;
+    public int Player3Luck = 35;
+
+    public int Player1Intelligence = 150;
+    public int Player2Intelligence = 170;
+    public int Player3Intelligence = 140;
+
+    public int Player1Special = 20;
+    public int Player2Special = 25;
+    public int Player3Special = 30;
 
     public void OnGUI()
     {
@@ -16,19 +39,22 @@ public class ImpGUI : MonoBehaviour
         if (GUI.Button(new Rect(10, 60, 200, 200), "Player One"))
         {
             print("player One Stats");
-            Labeltext = "Name: Marcus Ureta\n\rAttck: 10000\n\rDefense: 6969\n\rSpeed: 10001\n\rLuck: 100\r\nIntelligence: Infinite\r\nSpecial: Being Built Different";
+            Labeltext = $"Name: Marcus Ureta\n\rAttck: {Player1attack}\n\rDefense: {Player1Defense}\n\rSpeed: {Player1Speed}\n\rLuck: {Player1Luck}\r\nIntelligence: {Player1Intelligence}\r\nSpecial:{Player1Special}";
         }
         if (GUI.Button(new Rect(10, 280, 200, 200), "Player Two"))
         {
             print("player Two Stats");
-            Labeltext = "Name: Micheal George\n\rAttck: 69\n\rDefense: n + 1993 + r:\n\rSpeed: 150\n\rLuck: 1\r\nIntelligence: 90\r\nSpecial: Biryani";
+            Labeltext = $"Name: Micheal George\n\rAttck: {Player2attack}\n\rDefense: {Player2Defense}:\n\rSpeed: {Player2Speed}\n\rLuck: {Player2Luck}\r\nIntelligence: {Player2Intelligence}\r\nSpecial: {Player1Special}";
         }
         if (GUI.Button(new Rect(10, 500, 200, 200), "Player Three"))
         {
             print("player Three Stats");
-            Labeltext = "Name: Enzo Villarin\n\rAttck: -69\n\rDefense: -420\n\rSpeed: -69420\n\rLuck: -1000\r\nIntelligence: 10\r\nSpecial: 150"; 
+            Labeltext = $"Name: Enzo Villarin\n\rAttck: {Player3attack}\n\rDefense: {Player3Defense}\n\rSpeed: {Player3Speed}\n\rLuck: {Player3Luck}\r\nIntelligence: {Player3Intelligence}\r\nSpecial: {Player3Special}";
         }
+
 
         GUI.Label(new Rect(800, 300, 500, 500), Labeltext);
     }
-}
+       
+    }
+
